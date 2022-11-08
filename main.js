fcb6bd877a6a67610e68782a81dc4773cedd6944
Codebot47 <100170825+Codebot47@users.noreply.github.com>
2022-11-07 21:41:07 -0500

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-function startClassification()
-{
-  navigator.mediaDevices.getUserMedia({ audio: true});
-    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/Zo14ZxL7G/model.json', modelReady);
-}
